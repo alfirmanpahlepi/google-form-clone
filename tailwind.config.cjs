7f13014e1707aed90a5ccf78fcb82ca05c2bf75c
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2023-03-26 23:45:59 +0700

    chore: apply tailwind container

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,20 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          margin: '0 auto',
+          maxWidth: '100%',
+          '@screen lg': {
+            maxWidth: '1200px',
+          },
+        },
+      });
+    },
+  ],
 };
